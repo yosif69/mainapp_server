f9e774e2734e76f15ebc89667d261b84c36a5987
yosif69 <yosifmatar123@gmail.com>
2024-09-30 18:22:25 +0300

    servarMain

--- API/models/LogIn.js
+++ API/models/LogIn.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require("mongoose");
-// const bcrypt = require('bcryptjs');
 
 const LogInSchema = new Schema({
   email: {
@@ -18,20 +17,7 @@ const LogInSchema = new Schema({
   },
 });
 
-// تشفير كلمة المرور قبل حفظ المستخدم
-LogInSchema.pre("save", async function (next) {
-  if (!this.isModified("pass")) {
-    return next();
-  }
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.pass = await bcrypt.hash(this.pass, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
-
+// حذف التشفير من الكود
 const LogIn_MODEL = model("LogIn", LogInSchema);
 
 module.exports = LogIn_MODEL;
